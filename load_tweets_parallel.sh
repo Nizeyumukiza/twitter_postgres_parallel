753285fa3af5ed7bbc8149a12239239bd7b232fb
Nizeyumukiza <jpnizeyumukiza@gmail.com>
2023-04-11 21:18:06 -0700

    sequentil works, paraller normalized works

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -5,14 +5,15 @@ files=$(find data/*)
 echo '================================================================================'
 echo 'load pg_denormalized'
 echo '================================================================================'
-# FIXME: implement this
+echo "$files"
+time echo "$files" | parallel ./load_denormalized.sh
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-echo "$files" | time parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:2/ --inputs
+time echo "$files" | time parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:3500/ --inputs
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-echo "$files" | time parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:3/ --inputs
+time echo "$files" | time parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:4500/ --inputs
